cbb94d673666397eb8b96edebf88cbb93c2eaa13
Josh Kellhofer <sasukekorlo@yahoo.com>
2022-02-03 11:03:59 -0800

    Updated all files to UML status

---
+++ fleet.py
@@ -0,0 +1,7 @@
+class Fleet:
+
+  def __init__(self):
+    self.list = []
+
+    def create_fleet(self):
+      pass
---
+++ robot.py
@@ -0,0 +1,9 @@
+class Robot:
+
+  def __init__(self, name, health, weapon):
+    self.name = name
+    self.health = health
+    self.weapon = weapon
+
+    def attack(self, dinosaur):
+      pass
