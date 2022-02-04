051eac9cd2ecd7ec2efb276d6496e56721e23acd
Josh Kellhofer <sasukekorlo@yahoo.com>
2022-02-04 13:54:17 -0800

    Updated battlefield Class to fix calling herd and fleet in all functions to make them identical.

--- battlefield.py
+++ battlefield.py
@@ -11,10 +11,9 @@ class Battlefield:
     
   def run_game(self):
     
-    
     self.display_welcome()
     self.battle()
-
+   
   def display_welcome(self):
       print("WELCOME TO THE BATTLE OF BEAST VS. MACHINE\n")
       print(" Battling for dominance over the universe")
@@ -22,61 +21,62 @@ class Battlefield:
       print("    No one knows. Let's find out!") 
 
   def battle(self):
-      print(f"Your dinosaur's remaining: {Herd}")
-      print(f"Your robots remaining: {Fleet}")
-
+      
+      print(f"Your robots remaining: {self.fleet.fleet_list}")
+      print(f"Your dinosaur's remaining: {self.herd.herd_list}")
+      
       print("Robots are slow. Dinosaurs attack first!\n")
+      
       robot = random.choice(self.fleet.fleet_list)   
       dinosaur = random.choice(self.herd.herd_list)   
-      dino_turn
-      show_dino_opponent_options
-
       
+      self.show_dino_opponent_options()
+
       press_one = input(" ")
       if press_one == 1:
         dinosaur.attack(robot)
       else:
         print("Please press 1 to attack")
       if robot.self.health <= 0:
-        Fleet.self.list.remove(robot)
+        self.fleet.fleet_list.remove(robot)
         print(f"{robot} has been defeated!")
 
-        self.robot_turn
-        self.show_robot_opponent_options
+      self.robot_turn()
+      self.show_robot_opponent_options()
       
       press_one = input(" ")
       if press_one == 1:
-        robot.attack
+        robot.attack(dinosaur)
       else:
         print("Please press 1 to attack")
       if dinosaur.self.health <= 0:
-        Herd.self.list.remove(dinosaur)
+        self.herd.herd_list.remove(dinosaur)
         print(f"{dinosaur} has been defeated!")
 
-    
-        def dino_turn(self, dinosaur):
+  def dino_turn(self, dinosaur):
             self.dinosaur = dinosaur
-        print("It is now {dinosaur}'s turn to go")
+            print("It is now {dinosaur}'s turn to go")
 
-        def robo_turn(self, robot):
+  def robo_turn(self, robot):
             self.robot = robot
-        print("It is now {robot}'s turn to go ")
-
-        def show_dino_opponent_options(self):
+            print("It is now {robot}'s turn to go ")
+            
+  def show_dino_opponent_options(self):
             print("Press 1 to attack")
 
-        def show_robo_opponent_options(self):
+  def show_robo_opponent_options(self):
             print("Press 1 to attack")
-    
-
-        def display_winners(self):
-            if len(Fleet.self.list) <= 0:
-                print("Arrrr! the Dinosaurs have won!")
-            elif len(Herd.self.list) <= 0:
-                print("Robots now rule the Universe!") 
+                  
+  def display_winners(self):
+      if len(self.fleet.fleet_list) <= 0:
+        print("Arrrr! the Dinosaurs have won!")
+        print("       GAME OVER")
+      elif len(self.herd.herd_list) <= 0:
+        print("Robots now rule the Universe!") 
+        print("       GAME OVER")
             #end game
-            else: 
-                print("The battle rages on!")
-            pass
+      else: 
+        print("The battle rages on!")
+        pass
 
   
